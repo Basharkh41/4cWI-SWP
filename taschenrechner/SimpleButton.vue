4d28b7d8d2ca7ab7b5c9e51e4ff1b04ae0086d1e
Basharkh41 <104624006+Basharkh41@users.noreply.github.com>
2022-12-11 22:07:56 +0100

    Add files via upload

---
+++ taschenrechner/SimpleButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <button class="signs" @click="add">+</button>
+    <button class="signs" @click="subtract">-</button>
+    <button class="signs" @click="multi">*</button>
+    <button class="signs" @click="divide">/</button>
+    <button class="res" @click="result">=</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: [
+    "number-add",
+    "number-subtract",
+    "number-multi",
+    "number-divide",
+    "result",
+  ],
+
+  components: {},
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+    add() {
+      this.$emit("number-add");
+    },
+    subtract() {
+      this.$emit("number-subtract");
+    },
+    multi() {
+      this.$emit("number-multi");
+    },
+    divide() {
+      this.$emit("number-divide");
+    },
+    result() {
+      this.$emit("result");
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Georgia;
+  background-color: black;
+}
+.signs {
+  font-size: 20px;
+  padding: 20px 50px 20px 50px;
+  transition: 0.3s;
+}
+
+.res {
+  font-size: 20px;
+  padding: 20px 300px 20px 300px;
+  margin-top: 1.5rem;
+}
+
+button {
+  box-shadow: 5px 5px 10px rgb(83, 134, 83);
+}
+</style>
---
+++ taschenrechner/TheCalculator.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="parent">
+    <h1>Calculator</h1>
+    <input type="text" class="inputCalc" v-model="numberFromInput" />
+      <simple-button class="buttons"
+        @number-add="add"
+        @number-subtract="subtract"
+        @number-multi="multi"
+        @number-divide="divide"
+        @result="result"
+      ></simple-button>
+  </div>
+</template>
+<script>
+import SimpleButton from "./SimpleButton.vue";
+
+export default {
+  emits: ["resultNumber"],
+  name: "App",
+  props: {
+  },
+  components: {
+    SimpleButton,
+  },
+  data() {
+    return {
+      numberFromInput: '',
+      operator: "",
+      resultNumber: 0,
+    };
+  },
+
+  methods: {
+    add() {
+      this.numberFirst = this.numberFromInput;
+      this.operator = "+";
+      this.numberFromInput = '';
+    },
+    subtract() {
+      this.numberFirst = this.numberFromInput;
+      this.operator = "-";
+      this.numberFromInput = '';
+    },
+    multi() {
+      this.numberFirst = this.numberFromInput;
+      this.operator = "*";
+      this.numberFromInput = '';
+    },
+    divide() {
+      this.numberFirst = this.numberFromInput;
+      this.operator = "/";
+      this.numberFromInput = '';
+    },
+    result() {
+      this.numberSecond = this.numberFromInput;
+      switch (this.operator) {
+        case "+":
+          this.resultNumber =
+            parseInt(this.numberFirst) + parseInt(this.numberSecond);
+            this.numberFromInput = this.resultNumber
+          break;
+        case "-":
+          this.resultNumber =
+            parseInt(this.numberFirst) - parseInt(this.numberSecond);
+            this.numberFromInput = this.resultNumber
+          break;
+        case "*":
+          this.resultNumber =
+            parseInt(this.numberFirst) * parseInt(this.numberSecond);
+            this.numberFromInput = this.resultNumber
+          break;
+        case "/":
+          this.resultNumber =
+            parseInt(this.numberFirst) / parseInt(this.numberSecond);
+            this.numberFromInput = this.resultNumber
+          break;
+      }
+    },
+  },
+};
+</script>
+<style>
+.buttons {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+.parent {
+    background-color: rgb(20, 107, 20);
+  border-radius: 30px;
+  padding: 2em 1em 12em 1em;
+  margin: auto;
+  width: 292px;
+  clear: both;
+  display: table;
+}
+.inputCalc {
+  text-align: center;
+  height: 50px;
+  width: 610px;
+  margin-bottom: 1rem;
+  font-size: 30px;
+}
+h1 {
+  font-size: 4rem;
+}
+</style>
